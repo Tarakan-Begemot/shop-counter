4d26de63b6bac18edb506030c5006c060604e511
laird <laird@mail.com>
2022-06-20 19:19:05 +0200

    Updates

---
+++ precache-manifest.af4a305cb3174ae9fd95bc303db061c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b22724944be940a4e31",
+    "url": "/shop-counter/static/js/main.4b227249.chunk.js"
+  },
+  {
+    "revision": "890f6c79a4e5b572faca",
+    "url": "/shop-counter/static/css/1.72c7f232.chunk.css"
+  },
+  {
+    "revision": "890f6c79a4e5b572faca",
+    "url": "/shop-counter/static/js/1.890f6c79.chunk.js"
+  },
+  {
+    "revision": "b32a551c0d16a7bea5ec",
+    "url": "/shop-counter/static/js/runtime~main.b32a551c.js"
+  },
+  {
+    "revision": "dfd04272f1a559d55b2274b6d8517218",
+    "url": "/shop-counter/index.html"
+  }
+];
